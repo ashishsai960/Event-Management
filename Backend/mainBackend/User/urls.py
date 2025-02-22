85295a2f7d97809b2c8f2e33c165b1c5080b8833
Lakavath Ashish Sai Naik <96200707+ashishsai960@users.noreply.github.com>
2025-02-23 00:57:19 +0530

    minor chnges

--- Backend/mainBackend/User/urls.py
+++ Backend/mainBackend/User/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import VendorListByCategoryView
 
 urlpatterns = [
-    path('api/vendors/', VendorListByCategoryView.as_view(), name='vendor-list-by-category'),
+    path('vendors/', VendorListByCategoryView.as_view(), name='vendor-list-by-category'),
 ]
